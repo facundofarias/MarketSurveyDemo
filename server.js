8e36ef2a9cb0f108a51d64a5f6846f07594920e7
Facundo Farias <facundo.farias@gmail.com>
2014-02-13 11:16:29 -0300

    Minor changes

--- server.js
+++ server.js
@@ -2,8 +2,11 @@
 var hapi = require('hapi');
 var routes = require('./routes');
 
+var URL = '0.0.0.0';
+var PORT = 8080;
+
 var config = { };
-var server = new hapi.Server('0.0.0.0', 8080, config);
+var server = new hapi.Server(URL, PORT, config);
 server.pack.require({ lout: { endpoint: '/docs' } }, function (err) {
 
     if (err) {
@@ -14,12 +17,11 @@ server.pack.require({ lout: { endpoint: '/docs' } }, function (err) {
 server.addRoutes(routes);
 
 server.start();
-
-console.log("Server started at: http://localhost:8080/ ");
+console.log('Server started at: ' + URL + ':' +PORT);
 
 server.on('request', function (request, event, tags) {
 
-    console.log(request.path);
+    console.log('Received request: ' + request.path);
 
     if (tags.error) {
         console.log(event);
